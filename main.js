8106f30645c266bea597211ebd6e603ebcafbb0c
Aayush Karki <147123757+akarki2005@users.noreply.github.com>
2024-07-07 00:58:03 -0600

    added initial stuff

---
+++ main.js
@@ -0,0 +1,8 @@
+// Main JS File
+
+function showPage(page_id) {
+    // hide all divs
+    document.querySelectorAll('div').forEach(div => div.style.display = 'None')
+    // show selected div (based on page_id)
+    document.getElementById(page_id).style.display = 'block'
+}
