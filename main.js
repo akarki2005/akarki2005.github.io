62202b0d6c7d80706dd90226d6a573215bda95da
Aayush Karki <147123757+akarki2005@users.noreply.github.com>
2024-07-07 14:34:50 -0600

    alpha 0.1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 function showPage(page_id) {
     // hide all divs
-    document.querySelectorAll('div').forEach(div => div.style.display = 'None')
+    document.querySelectorAll('div.dynamic').forEach(div => div.style.display = 'None')
     // show selected div (based on page_id)
     document.getElementById(page_id).style.display = 'block'
 }
