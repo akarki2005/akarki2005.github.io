2d9646039ecd8d8000f84257fe25d4fa529cb911
Aayush Karki <147123757+akarki2005@users.noreply.github.com>
2024-10-21 04:14:33 -0400

    revamped design

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
 // Main JS File
-
-function showPage(page_id) {
-    // hide all divs
-    document.querySelectorAll('div.dynamic').forEach(div => div.style.display = 'None')
-    // show selected div (based on page_id)
-    document.getElementById(page_id).style.display = 'block'
-}
-
-function redirect(url) {
-    window.location.href = url
-}
